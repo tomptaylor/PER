e5d851da3d3c6ea82d15c458a0c4a07f0d05fd6e
Tom Taylor <tomptaylor@gmail.com>
2024-06-05 07:16:21 -0600

    got the tabstrips working, about to add map on middle tab

--- src/routes/tools.tsx
+++ src/routes/tools.tsx
@@ -1,26 +1,27 @@
 import { createSession, signOut } from "@solid-mediakit/auth/client";
 import { Show, type VoidComponent } from "solid-js";
-
+import Tab from "~/components/Tab";
 
 const Protected: VoidComponent = () => {
   const session = createSession();
 
-
-  
   return (
-    <Show when={session()} fallback={
-      <p>Only shown for logged in users</p>
-    } keyed>
-      {us => (
+    <Show
+      when={session()}
+      fallback={<p>Only shown for logged in users</p>}
+      keyed
+    >
+      {(us) => (
         <main>
-          <h1>Protected</h1>
+          <h1>Report</h1>
           {us.user?.image ? <img src={us.user?.image} /> : null}
           <span>Hey there {us.user?.name}! You are signed in!</span>
+          <Tab />
           <button
             onClick={() =>
               void signOut({
                 redirectTo: "/",
-                redirect: true
+                redirect: true,
               })
             }
           >
