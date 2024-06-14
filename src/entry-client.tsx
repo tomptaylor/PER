faa037d069896afdf266c1f4b8ad3a2b3f5f3980
Tom Taylor <tomptaylor@gmail.com>
2024-06-14 07:55:25 -0600

    update bg color

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,10 @@
 import { mount, StartClient } from "@solidjs/start/client";
 
+let temp = document.getElementById("app");
+temp?.classList.add(
+  "bg-gradient-to-tr",
+  "from-green-300",
+  "via-slate-100",
+  "to-yellow-200"
+);
 mount(() => <StartClient />, document.getElementById("app"));
